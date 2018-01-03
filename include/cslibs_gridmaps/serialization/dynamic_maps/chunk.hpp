5dac3361c44d7083c7d35a70f7b722c501e8d54b
Cornelia Schulz <cornelia.schulz@uni-tuebingen.de>
2018-01-03 15:40:34 +0100

    moved serialization of array to cslibs_math

--- include/cslibs_gridmaps/serialization/dynamic_maps/chunk.hpp
+++ include/cslibs_gridmaps/serialization/dynamic_maps/chunk.hpp
@@ -2,6 +2,7 @@
 #define CSLIBS_GRIDMAPS_SERIALIZATION_DYNAMIC_MAPS_CHUNK_HPP
 
 #include <cslibs_gridmaps/dynamic_maps/chunk.hpp>
+#include <cslibs_math/serialization/array.hpp>
 #include <yaml-cpp/yaml.h>
 
 namespace cslibs_gridmaps {
@@ -25,31 +26,6 @@ struct IndexedChunk {
 }
 
 namespace YAML {
-template <typename T, std::size_t Dim>
-struct convert<std::array<T, Dim>>
-{
-    static Node encode(const std::array<T, Dim> &rhs)
-    {
-        Node n;
-
-        for (std::size_t i = 0 ; i < Dim ; ++ i)
-            n.push_back(rhs[i]);
-
-        return n;
-    }
-
-    static bool decode(const Node& n, std::array<T, Dim> &rhs)
-    {
-        if (!n.IsSequence() || n.size() != Dim)
-            return false;
-
-        for (std::size_t i = 0 ; i < Dim ; ++ i)
-            rhs[i] = n[i].as<T>();
-
-        return true;
-    }
-};
-
 template <typename T>
 struct convert<cslibs_gridmaps::dynamic_maps::Chunk<T>>
 {
