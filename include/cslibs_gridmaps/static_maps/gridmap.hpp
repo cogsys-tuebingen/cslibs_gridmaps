e2f556b888f538069984138754ff264a9cc611c5
Richard Hanten <richard.hanten@uni-tuebingen.de>
2018-08-28 13:54:23 +0200

    rounding

--- include/cslibs_gridmaps/static_maps/gridmap.hpp
+++ include/cslibs_gridmaps/static_maps/gridmap.hpp
@@ -219,8 +219,8 @@ public:
     {
         const cslibs_math_2d::Point2d p_m = m_T_w_ * p_w;
 
-        i[0] = static_cast<int>(std::floor(p_m(0) * resolution_inv_));
-        i[1] = static_cast<int>(std::floor(p_m(1) * resolution_inv_));
+        i[0] = static_cast<int>(std::floor(p_m(0) * resolution_inv_ + 0.5));
+        i[1] = static_cast<int>(std::floor(p_m(1) * resolution_inv_ + 0.5));
 
         return (i[0] >= 0 && i[0] <= max_index_[0]) &&
                (i[1] >= 0 && i[1] <= max_index_[1]);
